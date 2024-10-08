be7833abc3586f8f9c31d08bcd4d8b35cedbfa46
Dorsch, Haden <hdorsch@iu.edu>
2024-10-08 10:48:34 -0400

    Made Variables in JS

---
+++ week7/app.js
@@ -0,0 +1,44 @@
+// Using Var makes a variable that can be reassigned and globally referenced
+
+var myName = "Haden";
+
+// Using let makes a variable that can be reassigned values
+
+let myTitle = "Student";
+
+// title change
+
+myTitle = "Graduate"
+
+// using const makes a variable that cant be changed
+
+const ssn = 999
+
+// Variable Types
+// Strings
+let str1 = "Blah Blah Blah"
+const s5 = "123"
+// Starting quote should be the same as ending quote. " cant end with ', and vice versa
+
+// Numbers
+let myAge = 22;
+const LessThanZero = -1;
+
+// Null/Undefined
+let myWife;
+
+// Arrays
+const myFavoriteColors = ["Red", "Blue", "Gray", "White", "Black"];
+const random = [True, 1, "hah", etc];
+
+// Objects are variables you need to describe
+const person = {
+    hairColor: "Black",
+    eyeColor: "Brown",
+    sayMyName() {},
+};
+
+// Function
+function doSomething() {
+    // Make function do something
+}
