95ed90059da86a500c8091b6a01d4d87c1ab688d
Dorsch, Haden <hdorsch@iu.edu>
2024-10-08 11:29:13 -0400

    finished java basics

---
+++ week7/console.js
@@ -0,0 +1,16 @@
+console.log("hi");
+
+const myAge = 22;
+
+console.log(myAge);
+
+let myDog = {
+    hairColor: "brown",
+    weight: 90,
+    breed: "King Corso",
+    disposition: "Emotional",
+    name: "Ragnar",
+}
+
+console.log(myDog);
+
