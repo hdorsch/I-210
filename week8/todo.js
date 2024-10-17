73fd4808a024fb685555c2eca08bcbc8ad09cd28
Dorsch, Haden <hdorsch@iu.edu>
2024-10-17 11:32:19 -0400

    stuff

---
+++ week8/todo.js
@@ -0,0 +1,20 @@
+console.log("Howdy");
+
+const todoList = [];
+
+todoList.push("Sweep the Cieling");
+
+todoList.push("Mow the carpet");
+
+todoList.push("Cook the dishes");
+
+console.log (todoList);
+
+todoList.pop();
+
+console.log (todoList);
+
+for (let i = 0; i < todoList.length; i++) {
+    console.log[i];
+    console.log(todoList[i]);
+}
