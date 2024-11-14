6a9b56cab8a4f92ea6f317271836f531252a6ba7
Dorsch, Haden <hdorsch@iu.edu>
2024-11-14 11:10:55 -0500

    updated firebase

--- week12/connectfirebase.js
+++ week12/connectfirebase.js
@@ -1,5 +1,5 @@
 import { app } from "./firebase.js";
-import { getFirestore, collection, getDocs, query } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import { getFirestore, collection, getDocs, addDoc, query } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
 const booksListRef = document.querySelector("#BooksList")
 
@@ -9,6 +9,7 @@ async function getBooks() {
     const booksCollection = collection(db, "Books");
     const booksDocs = await getDocs(booksCollection);
     
+    booksListRef.innerHTML = "";
     for (let i = 0; i < booksDocs.docs.length; i++) {
         const book = booksDocs.docs[i];
         const bookData = book.data();
@@ -17,6 +18,17 @@ async function getBooks() {
     }
 }
 
+async function addBook() {
+    const bookName = prompt("Book Name:");
+
+    const booksCollection = collection(db, "Books");
+
+    await addDoc(booksCollection, { title: bookName});
+
+    getBooks();
+}
+
+document.querySelector("#addBookButton").onclick = addBook;
 getBooks();
 
 console.log(db);
